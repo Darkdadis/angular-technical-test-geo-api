cffdbe65e96bac59f85e23a83d2f18da20c97522
Régis Buquet <rbuquet@assist-evolution.com>
2025-07-16 15:27:37 +0200

    feat: add test to check searchRegion function

--- src/app/services/geo-api.service.spec.ts
+++ src/app/services/geo-api.service.spec.ts
@@ -1,24 +1,44 @@
-import { TestBed } from '@angular/core/testing';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
-
-import { GeoApi } from './geo-api.service';
-import {HttpClient, HttpHandler} from '@angular/common/http';
+import {TestBed} from '@angular/core/testing';
+import {HttpTestingController, provideHttpClientTesting} from '@angular/common/http/testing';
+import {GeoApi} from './geo-api.service';
+import {provideHttpClient} from '@angular/common/http';
+import {RegionModel} from '../models/region.model';
 
 describe('GeoApi', () => {
   let service: GeoApi;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
+        provideHttpClient(),
         provideHttpClientTesting(),
-        HttpClient,
-        HttpHandler,
       ]
     });
     service = TestBed.inject(GeoApi);
+      httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    afterEach(() => {
+        httpMock.verify();
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should fetch regions correctly from the API', () => {
+      const mockRegions: RegionModel[] = [
+          {code: '1', nom: 'Normandie', score: 10}
+      ];
+      const regionName = 'Normandie';
+
+      service.searchRegion(regionName);
+      const req = httpMock.expectOne(`https://geo.api.gouv.fr/regions?nom=${encodeURIComponent(regionName)}`);
+      expect(req.request.method).toBe('GET');
+
+      req.flush(mockRegions);
+
+      expect(service.regions()).toEqual(mockRegions);
+    });
 });
